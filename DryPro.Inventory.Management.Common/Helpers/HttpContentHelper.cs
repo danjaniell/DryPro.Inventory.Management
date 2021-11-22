9aa2befeb42715ed24c75101a977d5b7d8c6eccc
Jai <iaj.paragas@yahoo.com>
2021-11-22 14:21:03 +0800

    Added Views. Create product done

---
+++ DryPro.Inventory.Management.Common/Helpers/HttpContentHelper.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace DryPro.Inventory.Management.Common.Helpers
+{
+    public class HttpContentHelper
+    {
+        public static HttpContent CreateRequest(object request)
+        {
+            var jsonRequest = JsonConvert.SerializeObject(request);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(jsonRequest);
+            var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            return byteContent;
+        }
+    }
+}
---
+++ DryPro.Inventory.Management.UI/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using DryPro.Inventory.Management.Application.Commands;
+using DryPro.Inventory.Management.Application.Mappers;
+using DryPro.Inventory.Management.Common.Helpers;
+using DryPro.Inventory.Management.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace DryPro.Inventory.Management.UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var products = new List<Product>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:5001/api/Product/GetAll"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    products = JsonConvert.DeserializeObject<List<Product>>(apiResponse);
+                }
+            }
+            return View(products);
+        }        
+        
+        // GET: Product/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind(nameof(Product.Id),
+                                                nameof(Product.Type),
+                                                nameof(Product.Color),
+                                                nameof(Product.SellingPrice),
+                                                nameof(Product.SoldPrice),
+                                                nameof(Product.Cost),
+                                                nameof(Product.Discount),
+                                                nameof(Product.AuxilliaryItems))] CreateProductCommand command)
+        {
+            Product result = null;
+            if (ModelState.IsValid)
+            {
+                //var command = ProductMapper.Mapper.Map<CreateProductCommand>(product);
+                HttpContent request = HttpContentHelper.CreateRequest(command);
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.PostAsync("https://localhost:5001/api/Product/Add", request))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        result = JsonConvert.DeserializeObject<Product>(apiResponse);
+                    }
+                }
+            }
+            return View(result);
+        }
+    }
+}
