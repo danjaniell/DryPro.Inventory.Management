198756079c7228a80e2da50c28f87a2557dde982
Jai <iaj.paragas@yahoo.com>
2021-12-01 14:12:38 +0800

    Navbar changes included shop links, removed Home-Contact page

--- DryPro.Inventory.Management.UI/Controllers/HomeController.cs
+++ DryPro.Inventory.Management.UI/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace DryPro.Inventory.Management.UI.Controllers
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
