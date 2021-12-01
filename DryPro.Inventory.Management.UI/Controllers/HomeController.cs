0e8e40b91cb185dfbdc89e39604df0526b03ac5b
Jai <iaj.paragas@yahoo.com>
2021-12-01 11:06:25 +0800

    Navbar changes, LogIn still not aligned right

--- DryPro.Inventory.Management.UI/Controllers/HomeController.cs
+++ DryPro.Inventory.Management.UI/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace DryPro.Inventory.Management.UI.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
