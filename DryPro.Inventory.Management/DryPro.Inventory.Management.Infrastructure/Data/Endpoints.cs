95a06623366e6130ae80dc41d55141c23ef9ccbe
Jai <iaj.paragas@yahoo.com>
2021-12-08 20:01:14 +0800

    Moved Common Project location

---
+++ DryPro.Inventory.Management/DryPro.Inventory.Management.Infrastructure/Data/Endpoints.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DryPro.Inventory.Management.Infrastructure.Data
+{
+    public interface IEndpoint
+    {
+        KeyValuePair<string, string> Value { get; set; }
+    }
+
+    public class Endpoints : IEndpoint
+    {
+        public KeyValuePair<string, string> Value { get; set; }
+    }
+}
