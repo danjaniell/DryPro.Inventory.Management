8ffa1ebcf0e615f593f0ba95ac0ff151c1b79fc4
Jai <iaj.paragas@yahoo.com>
2021-12-09 14:03:44 +0800

    Add Test appsettings

--- DryPro.Inventory.Management/DryPro.Inventory.Management/Program.cs
+++ DryPro.Inventory.Management/DryPro.Inventory.Management/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace DryPro.Inventory.Management
@@ -12,6 +13,11 @@ namespace DryPro.Inventory.Management
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: false)
+                          .AddJsonFile("appsettings.Test.json", optional: false, reloadOnChange: false);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
