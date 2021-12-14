15e3643ad72b21077139ea0bff33f843cb90a0e3
Jai <iaj.paragas@yahoo.com>
2021-12-14 15:20:22 +0800

    Create Inventory Page

---
+++ DryPro.Inventory.Management/DryPro.Inventory.Management.UI/Controllers/InventoryController.cs
@@ -0,0 +1,30 @@
+﻿using DryPro.Inventory.Management.Application.Commands;
+using DryPro.Inventory.Management.Application.Mappers;
+using DryPro.Inventory.Management.Common.Helpers;
+using DryPro.Inventory.Management.Core.Entities;
+using DryPro.Inventory.Management.Infrastructure.Data;
+using DryPro.Inventory.Management.UI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DryPro.Inventory.Management.UI.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private readonly IEndpoint _ep;
+
+        public InventoryController(IEndpoint ep)
+        {
+            _ep = ep;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
