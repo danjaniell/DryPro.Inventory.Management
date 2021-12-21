bc634456ce5f9d7b11e91682b0272421714f5760
Jai <iaj.paragas@yahoo.com>
2021-12-21 21:56:40 +0800

    View pages reskinning

--- DryPro.Inventory.Management/DryPro.Inventory.Management.Core/Entities/Product.cs
+++ DryPro.Inventory.Management/DryPro.Inventory.Management.Core/Entities/Product.cs
@@ -2,6 +2,7 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -15,9 +16,12 @@ namespace DryPro.Inventory.Management.Core.Entities
         public string _id { get; set; }
         public ProductType Type { get; set; }
         public ProductColor Color { get; set; }
+        [DisplayName("Selling Price")]
         public decimal SellingPrice { get; set; }
+        [DisplayName("Sold Price")]
         public decimal SoldPrice { get; set; }
         public decimal Cost { get; set; }
+        [DisplayName("Total Cost")]
         public decimal TotalCost => Cost + AuxilliaryItems.Sum(x => x.Cost);
         public decimal Discount { get; set; }
         public IList<AuxilliaryItem> AuxilliaryItems { get; set; } = new List<AuxilliaryItem>();
